9acc0ccd8caf85a49a919e69d35dfba4ff495d75
Gregory Mahnke <g.mahnke@neusta.de>
2020-05-07 17:43:05 +0200

    Add Netlify CMS

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
         ],
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-        ]
+        ],
+        script: [
+            {src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'}
+        ],
     },
     /*
     ** Customize the progress-bar color
