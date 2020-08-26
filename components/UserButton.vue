687049abbfa2e315b6081190f13d355082569f9e
Gregory Mahnke <g.mahnke@neusta.de>
2020-08-26 16:56:20 +0200

    Add UserMenu

--- components/UserButton.vue
+++ components/UserButton.vue
@@ -1,13 +1,23 @@
 <template>
     <div class="md:col-end-5 flex items-center md:justify-end">
         <client-only>
-            <template v-if="fetchedUser">
-                <div class="">Hi {{ fetchedUser.user_metadata.full_name }}</div>
-                <button onclick="netlifyIdentity.logout()" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-auto md:ml-2">
-                    Logout
+            <div class="relative w-full md:w-auto" v-if="fetchedUser">
+                <button v-on:click="hideUserMenu = !hideUserMenu" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full">
+                    <span class="mr-1">Hi</span><span class="font-bold">{{ fetchedUser.user_metadata.full_name }}</span>
                 </button>
+                <div v-bind:class="{ hidden: hideUserMenu }" id="userMenu" class="origin-top-right md:absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                    <ul class="rounded-md bg-white shadow-xs p-2">
+                        <li><a href="/admin/index.html">admin</a></li>
+                        <li><nuxt-link to="profile">profile</nuxt-link></li>
+                        <li>
+                            <button onclick="netlifyIdentity.open()" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded">
+                                Logout
+                            </button>
+                        </li>
+                    </ul>
 
-            </template>
+                </div>
+            </div>
             <button v-else onclick="netlifyIdentity.open()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto">
                 Login
             </button>
@@ -23,7 +33,8 @@
 
         data() {
             return {
-                fetchedUser: ''
+                fetchedUser: '',
+                hideUserMenu: true
             }
         },
 
